f79d489575b6ee2ad8beebb35c765c4702753d97
heimgun <heidi.guneriussen@live.no>
2020-05-13 19:57:22 +0200

    Async-connection to API

--- app/src/main/java/com/example/conferencebookingapp/MainActivity.java
+++ app/src/main/java/com/example/conferencebookingapp/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.conferencebookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,28 @@ public class MainActivity extends AppCompatActivity {
         Button createUser, searchForRooms;
 
 
+        //Not sure if this should be a class or not, but is supposed to connect to the API
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                //Network-configurations
+                try {
+                    //URL
+                    URL timeToMeetEndPoint = new URL("http://dev-be.timetomeet.se/service/rest/");
+
+                    //Connection
+                    HttpURLConnection con = (HttpURLConnection) timeToMeetEndPoint.openConnection();
+
+                } catch (MalformedURLException e) {
+                    System.out.println("URL fail");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    System.out.println("Connection fail");
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
 
     }
